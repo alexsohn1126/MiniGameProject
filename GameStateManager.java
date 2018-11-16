84add8f24bfba82e2793cbc6fb45147a060a34c6
Alex Sohn <44201357+MashTaco@users.noreply.github.com>
2018-11-15 19:12:51 -0500

    Create GameStateManager.java
    Where game states (menu, level1, choice, etc) is pushed into stack

---
+++ GameStateManager.java
@@ -0,0 +1,31 @@
+package game.gamestate;
+
+import game.gamestate.GameState;
+import game.gamestate.MenuState;
+import java.awt.Graphics;
+import java.util.Stack;
+
+public class GameStateManager {
+    private Stack<GameState> states;
+    
+    public GameStateManager(){
+        states = new Stack<GameState>();
+        states.push(new MenuState(this));
+    }
+    
+    public void tick(){
+        states.peek().tick();
+    }
+    
+    public void draw(Graphics g){
+        states.peek().draw(g);
+    }
+    
+    public void keyPressed(int k){
+        states.peek().keyPressed(k);
+    }
+    
+    public void keyReleased(int k){
+        states.peek().keyReleased(k);
+    }
+}
