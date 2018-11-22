545a61a0c757ef044fe3749c032c8ada52263bda
Alex Sohn <44201357+MashTaco@users.noreply.github.com>
2018-11-21 21:49:45 -0500

    Update and rename Code/OptionState1.java to Code/game.gamestate/OptionState1.java

--- Code/game.gamestate/OptionState1.java
+++ Code/game.gamestate/OptionState1.java
@@ -1,15 +1,17 @@
-
 package game.gamestate;
 
 import game.GamePanel;
+import game.resources.Images;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.event.KeyEvent;
 
 public class OptionState1 extends GameState{
-    private String ques="Do you like skyline or ground";
-     private String[] options = {"Skyline","Ground"};
+    private final String ques="Do you like cities or forest";
+     private final String[] options = {"Cities","Forest"};
     private int currentSelection = 0;
     public OptionState1(GameStateManager gsm) {
         super(gsm);
@@ -21,22 +23,32 @@ public class OptionState1 extends GameState{
 
     public void draw(Graphics g) {
         
-        g.setColor(new Color(50,150,200));
-        g.fillRect(0,0, GamePanel.WIDTH, GamePanel.HEIGHT);
+        Graphics2D g2 = (Graphics2D)g;
+                
+        g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        
+        if(0 == currentSelection){
+            g2.drawImage(Images.blocks[8],0, 0, 900, 600, null);
+        } else {
+            g2.drawImage(Images.blocks[9],0, 0, 900, 600, null);
+        }
+        
         for (int i = 0; i < options.length; i++){
             if(i == currentSelection){
-                g.setColor(Color.GREEN);
+                g2.setColor(Color.GREEN);
             } else {
-                g.setColor(Color.WHITE);
+                g2.setColor(Color.WHITE);
+                
             }
             
             //g.drawLine(GamePanel.WIDTH/2, 0, GamePanel.WIDTH/2, GamePanel.HEIGHT);
-            g.setFont(new Font("Arial", Font.PLAIN, 72));
-            g.drawString(options[i], GamePanel.WIDTH/2+10, 300+ i*150);
+            g2.setFont(new Font("Arial", Font.PLAIN, 72));
+            g2.drawString(options[i], GamePanel.WIDTH/2-100, 300+ i*150);
         }
-        g.setColor(Color.WHITE);
-        g.setFont(new Font("Comic Sans MS", Font.PLAIN, 45));
-        g.drawString(ques, GamePanel.WIDTH/2-300, GamePanel.HEIGHT/2-50);
+        
+        g2.setColor(Color.WHITE);
+        g2.setFont(new Font("Comic Sans MS", Font.PLAIN, 45));
+        g2.drawString(ques, GamePanel.WIDTH/2-300, GamePanel.HEIGHT/2-100);
     }
 
     public void keyPressed(int k) {
@@ -55,10 +67,10 @@ public class OptionState1 extends GameState{
         if(k == KeyEvent.VK_ENTER){
             switch (currentSelection) {
                 case 0:
-                    gsm.states.push(new Level1State(gsm));
+                    gsm.states.push(new CityState(gsm));
                     break;
                 case 1:
-                     gsm.states.push(new MenuState(gsm));
+                     gsm.states.push(new ForestState(gsm));
                     break;
                 default:
                     break;
