da645a874557f4623343102c2a0dfc54cbc9cca0
Mike Kagnew <44201432+FinesseMaster@users.noreply.github.com>
2018-11-24 16:52:01 -0500

    The lava level

---
+++ Code/game.gamestate/LavaState.java
@@ -0,0 +1,54 @@
+package game.gamestate;
+
+import game.entities.Player;
+import static game.gamestate.GameState.xOffset;
+import static game.gamestate.GameState.yOffset;
+import game.mapping.Map;
+import game.objects.Block;
+import game.resources.Images;
+import java.awt.Graphics;
+
+public class LavaState extends GameState{
+    
+    private Player player;
+    private Map map;
+    
+    public LavaState(GameStateManager gsm){
+        super(gsm);
+    }
+
+    public void init() {
+        player = new Player(30,30);
+        map = new Map ("/Maps/map4.map");
+        
+        xOffset = -350;
+        yOffset = -100;
+    }
+    
+    public void tick() {
+        player.tick(map.getBlocks());
+        
+        if(yOffset >= 400){
+            heartLeft -= 1;
+            if(heartLeft == 0) gsm.states.push(new DeathState(gsm));
+            yOffset = -200;
+            xOffset = -350;
+        } else if (xOffset >= Map.getWidth() * Block.getBlockSize()-(2*Block.blockSize + Block.blockSize/2) - 385){
+            gsm.states.push(new OptionState3(gsm));
+        }
+    }
+    
+    public void draw(Graphics g) {
+        g.drawImage(Images.blocks[11], 0,0, 900,600, null);
+        map.draw(g);
+        player.draw(g);
+    }
+    
+    public void keyPressed(int k) {
+        player.keyPressed(k);
+    }
+    
+    public void keyReleased(int k) {
+        player.keyReleased(k);
+    }
+}
